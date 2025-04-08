61a4ee7e22bc9cafdd906f0b31bda0c73260313c
vanshikark <vanshikrjk11@gamil.com>
2025-04-08 19:11:37 +0530

    hehe

---
+++ asmd.java
@@ -0,0 +1,12 @@
+public class asmd {
+    public static void main(String[] args){
+        int a=20;
+        int b =40;
+        System.out.println(a+b);
+        System.out.println(b-a);
+        System.out.println(a*b);
+        System.out.println(b/a);
+
+    }
+    
+}
