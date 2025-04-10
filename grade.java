37ee108094c3c11abb84276d2205bdb1fd7c2065
vanshikark <vanshikrjk11@gamil.com>
2025-04-10 19:31:31 +0530

    hehe

---
+++ grade.java
@@ -0,0 +1,22 @@
+public class grade {
+    public static void main(String[] args){
+        int marks=85;
+        if(marks>=90)
+        {
+            System.out.println("Grade is A");
+        }
+        else if(marks>=85)
+        {
+            System.out.println("Grade is B");
+        }
+        else if(marks>=60)
+        {
+            System.out.println("Grade is C");
+        }
+        else
+        {
+            System.out.println("Fail");
+        }
+    }
+    
+}
