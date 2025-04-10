c94402dd73924e2ca7eb9f92d6814b4c5a3e9244
vanshikark <vanshikrjk11@gamil.com>
2025-04-10 18:59:37 +0530

    hehe

---
+++ vowel.java
@@ -0,0 +1,14 @@
+public class vowel {
+    public static void main(String[] args) {
+        char ch='c';
+        if(ch=='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        {
+            System.out.println("it is a vowel");
+        }
+        else
+        {
+            System.out.println("it is a consonent");
+        }
+    }
+    
+}
