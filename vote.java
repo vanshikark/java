95b806e397b5353c8f21a371c414543e13760325
vanshikark <vanshikrjk11@gamil.com>
2025-04-10 19:14:07 +0530

    hehe

---
+++ vote.java
@@ -0,0 +1,17 @@
+public class vote {
+        public static void main(String[] args) {
+            int age=20;
+            if(age>=18)
+            {
+                System.out.println("Eligible to vote");
+            }
+            else
+            {
+                System.out.println("Not eligble to vote");
+            }
+
+        }
+
+    }
+    
+
