7eed84597bf0e896c834b714205f737aa28d4762
vanshikark <vanshikrjk11@gamil.com>
2025-04-10 18:36:16 +0530

    hehe

---
+++ threelargest.java
@@ -0,0 +1,18 @@
+public class threelargest {
+    public static void main(String[] args) {
+        int a=50,b=90,c=70;
+        if(a>b && a>c)
+        {
+            System.out.println("a is greater");
+        }
+        else if(b>c)
+        {
+            System.out.println("b is greater");
+        }
+        else
+        {
+            System.out.println("c is greater");
+        }
+    }
+    
+}
